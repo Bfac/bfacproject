459693fbb539fc0bde80d64b0300bcb59e32c260
opclient <30980283+opclient@users.noreply.github.com>
2017-12-16 08:02:42 +0300

    asd

---
+++ main.js
@@ -0,0 +1,33 @@
+var ws = {};
+window.loadTime = null;
+function pdata(data) {
+    return new DataView(new ArrayBuffer(data));
+}
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+function connect() {
+    ws = new WebSocket("ws://194.87.95.4:8081");
+	ws.binaryType = "arraybuffer";
+    ws.onopen = function (arg) {
+        ws.send(JSON.stringify({
+            data: "getUserStatus",
+            userip: httpGet("https://api.ipify.org/")
+        }));
+	}
+	ws.onclose = function(arg) {
+		connect();
+	}
+	ws.onmessage = function(message) {
+		var msg = JSON.parse(message.data);
+        console.log(msg.data);
+        document.getElementById("userip").innerHTML = msg.reason.ip;
+        document.getElementById("usertime").innerHTML = (msg.reason.time / 3600 >> 0) + ":" + (msg.reason.time / 60 % 60 >> 0) + ":" + (msg.reason.time % 60 >> 0);
+        document.getElementById("userpackage").innerHTML = msg.reason.package;
+        document.getElementById("usermaxbots").innerHTML = msg.reason.maxbots;
+	}
+}
+connect();
